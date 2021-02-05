3393e5e7f32753766d8ba1b9d40a2875694acd48
vivimortensen <vivi4531@stud.kea.dk>
2021-02-05 11:29:20 +0100

    committed basics

---
+++ basics.js
@@ -0,0 +1,13 @@
+"use strict";
+y= 14; 
+
+const bool = true; 
+const num = 41; 
+const str ="Vivi";
+const obj = {
+    cats: 2, cars: 1
+};
+
+const nothing = null; 
+let undf; 
+const symbol = Symbol("symbol");
