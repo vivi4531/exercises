c24d560f5934ba780f365a065f0f3a38748fce6a
vivimortensen <vivi4531@stud.kea.dk>
2021-02-07 11:36:37 +0100

    exercise files added

---
+++ strings.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//Strings 5.3 
+const longline = "A very, very, very, very, VERY! long line of text,\n that spans across the width of the editor!";
+
+//Template literals 
+const name = "Peter";
+const drink = "Pepsi"; 
+
+const text = `Hello ${name}, would you like a ${drink}?`;
+
+console.log(text);
---
+++ template.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Strings 5.3 small template literal exercise 
+
+const name = "Vivi";
+const animal = "dog"; 
+const animalname = "Monty";
+
+console.log(`My name is ${name}.\nI have a ${animal} called ${animalname}.`);
+
+//Changed the variables and it works.
