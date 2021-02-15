931c13ee3a4fb844f62ee14eb2726b150ac3dca5
vivimortensen <vivi4531@stud.kea.dk>
2021-02-15 11:12:05 +0100

    1502 Name-parts function

---
+++ nameparts1.js
@@ -0,0 +1,55 @@
+"use strict"; 
+
+//1502 - Name-parts function
+
+function getNameParts(fullname){
+
+    //const fullname = "Peter Heronimous Lind"; 
+    const firstSpace = fullname.indexOf(" ");
+    const lastSpace = fullname.lastIndexOf(" ");
+    
+    const firstname = fullname.substring(0,firstSpace); 
+    const middlename = fullname.substring(firstSpace, lastSpace); 
+    const lastname = fullname.substring(lastSpace); 
+
+    
+    console.log(`${firstname}${middlename}${lastname}`);
+
+}
+
+getNameParts("vivi hermione mortensen");
+
+
+
+
+
+
+
+//Old way
+/*const fullname = "Peter Heronimous Lind"; 
+const firstname = fullname.substring(0,5);
+const middlename = fullname.substring(6,16);
+const lastname = fullname.substring(17,21); */
+
+//console.log(fullname);
+/* console.log(`full name is: _${fullname}_`);
+console.log(`first name is: _${firstname}_`);
+console.log(`middle name is: _${middlename}_`);
+console.log(`last name is: _${lastname}_`);
+ 
+//Using .split()
+console.log(`${fullname.split(" ")}`);*/
+
+//New way 
+//Using .indexOf() 
+// const fullname = "Peter Heronimous Lind"; 
+// const firstSpace = fullname.indexOf(" ");
+// const lastSpace = fullname.lastIndexOf(" ");
+
+// const firstname = fullname.substring(0,firstSpace); 
+// const middlename = fullname.substring(firstSpace, lastSpace); 
+// const lastname = fullname.substring(lastSpace); 
+
+// console.log(`First name is ${firstname}. Middle name is ${middlename}. Last name is ${lastname}.`);
+
+
